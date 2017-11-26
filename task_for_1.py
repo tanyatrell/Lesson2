8badc0bd6a1ef8aee9f2627247f2b3cba82cf378
Tanya Zatsepa <tzatsepa@gmail.com>
2017-11-26 12:28:01 +0300

    Изменила имена переменных

--- task_for_1.py
+++ task_for_1.py
@@ -11,8 +11,9 @@ all_scores = [
 {'school_class': '4в', 
 'scores': [3,5,2,4,4]}]
 
-result_school =0 
-result_class = 0
+result_school = 0 
+sum_classes = 0
+result = 0
 
 def avr_list(a_list):
     a_sum = 0
@@ -22,9 +23,9 @@ def avr_list(a_list):
 
 for each in all_scores:
     result = avr_list(each.get('scores'))
-    result_class = result_class + result
+    sum_classes = sum_classes + result
     print('Средняя по классу {}: {}'.format(each.get('school_class'),result))
 
-result_school = result_class/3
+result_school = round(sum_classes/3, 1)
 print('Средняя по школе: {}'.format(result_school))
 
